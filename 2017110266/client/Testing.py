f28d52379a431bb8dfbc1957ee8759b8cbf419ba
SeongHyukJang <seonghyuk578@khu.ac.kr>
2020-03-25 22:47:29 +0900

    server & client

--- 2017110266/client/Testing.py
+++ 2017110266/client/Testing.py
@@ -13,9 +13,3 @@ import requests
 
 # requests를 이용한 url 접근 속도 측정
 test_for_requests = requests.get('http://localhost:8080')
-
-# modules = { "http" : test_for_http,
-#             "urllib" : test_for_urllib,
-#             "requests" : test_for_requests
-# }
-
